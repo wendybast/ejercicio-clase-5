33cb45b71c73d0b2718b8631f7fac6458080c6b9
wendybast <wenbastidas@gmail.com>
2020-05-24 20:26:38 -0300

    primera parte

---
+++ clase5.java
@@ -0,0 +1,24 @@
+public class clase5{
+	public static void main(String args[]){
+		//1
+		int numero1=10;
+		//2
+		int numero2=6;
+
+	//	int suma=numero1+numero2;
+
+		System.out.println("El resultado es "+(numero1+numero2));
+           //3
+           float decimal1=1.34f;
+
+           System.out.println("decimal1");
+
+
+
+		
+	}
+}
+
+		
+		
+		
