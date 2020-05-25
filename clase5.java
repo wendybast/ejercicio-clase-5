ffc82a74602b451866373fe7baaed0b1633912be
wendybast <wenbastidas@gmail.com>
2020-05-25 03:19:36 -0300

    ejercicios resueltos

--- clase5.java
+++ clase5.java
@@ -2,16 +2,57 @@ public class clase5{
 	public static void main(String args[]){
 		//1
 		int numero1=10;
-		//2
+		//2 
 		int numero2=6;
 
 	//	int suma=numero1+numero2;
 
 		System.out.println("El resultado es "+(numero1+numero2));
-           //3
-           float decimal1=1.34f;
 
-           System.out.println("decimal1");
+//3
+           float decimal1=1.34f;
+//4
+           char caracter1='f';
+//5
+           boolean esVerdadero=true;
+//6
+          float suma=numero1+decimal1;
+
+          System.out.println("El resultado es "+ (numero1+decimal1));
+//7			
+/* si es posible. la computadora para resolver la suma, analiza el tipo de dato de la izquierda 
+conviertiendo el char a su valor ascii (f=102) para resolver la suma, como si fueran enteros*/
+
+		System.out.println("El resultado es "+(numero1+caracter1));
+//8
+			System.out.println("El resto es "+(numero1%numero2));
+//9
+			String cadena1="Hola Mundo";
+			
+			System.out.println(cadena1);
+//10 
+			int NUMERO_CONSTANTE=2;
+			System.out.println("El resultado es "+(NUMERO_CONSTANTE));
+//11        si, se puede hacerr
+			NUMERO_CONSTANTE=5;
+			System.out.println("El resultado es "+(NUMERO_CONSTANTE));
+/*12     si se puede hacer, el tipo de dato es de entrada numerica, en la linea 37 se declaro 
+que valia 5 y eso arrojo en consola*/  
+			numero2=NUMERO_CONSTANTE;
+			System.out.println(numero2);
+			
+		
+		
+			
+			
+			
+		
+
+
+
+
+         
+
 
 
 
